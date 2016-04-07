d292f9b8b51e616bfa9b3f70b132c282eea84355
agathe <agathe.blaise@isen-lille.fr>
2016-04-07 20:46:06 +0200

    Permits to recover outputs from several mics launched at the same time

--- ps2.py
+++ ps2.py
@@ -1,7 +1,7 @@
 # #!/usr/bin/env python2
 # # -*-coding:Latin-1 -*
 
-import commands, time, subprocess, signal, os, pyaudio, sys, wave
+import subprocess, pyaudio, multiprocessing.pool
 from subprocess import Popen
 
 listDevices = list()
@@ -20,32 +20,17 @@ for i in range(max_devs):
 		if name == 'name' and 'Samson' in value :
 			listDevices.append(i)
 
-# pour chacun des micros
 numberDevices = len(listDevices)
-print(numberDevices)
 
-for i in listDevices:
-	print(i)
+processes = []
 
-dic = {}
-pDic = {}
-var = 1
-if (numberDevices > 1) :
-	for dev in listDevices :
-		if var == 1 :
-	 		dic["string{0}".format(dev)] = ["python", "runMic.py", str(dev)]
-			pDic["string{0}".format(dev)] = subprocess.Popen(dic["string{0}".format(dev)])
-			var = 2
-		else :
-			dic["string{0}".format(dev)] = ["python", "runMic1.py", str(dev)]
-			pDic["string{0}".format(dev)] = subprocess.Popen(dic["string{0}".format(dev)])
-elif (numberDevices == 1) :
- 	dev0 = ["python", "runMic.py", str(listDevices[0])]
- 	pDev0 = subprocess.Popen(dev0)
+pool = multiprocessing.pool.ThreadPool(numberDevices)
+for dev in listDevices :
+	processes.append(lambda dev=dev: subprocess.check_output((["python", "runMic.py", str(dev)])))
 
-# dev0 = ["python", "runMic.py", "3"]
-# dev1 = ["python", "runMic1.py", "4"]
-# pDev0 = subprocess.Popen(dev0)
-# pDev1 = subprocess.Popen(dev1)
+outputs = pool.map(lambda x: x(), processes)
+
+for o in outputs :
+	print o
 
 p.terminate()
